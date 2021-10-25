87c1224e954e86f8e72b2ea4dda280f8986e3e42
Jakob J. Kolb <kolb@hardfork.io>
2021-10-25 15:01:03 +0200

    Add db schema for post processes answers.

---
+++ dags/database/migrations/migration_files/20211025_00_add_datenspende_derivatives_schema.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA datenspende_derivatives;
+
+CREATE TABLE datenspende_derivatives.test_and_symptoms_answers (
+    user_id INT,
+    question_id INT,
+    answers VARCHAR,
+    created_at BIGINT,
+    PRIMARY KEY (user_id, question_id, created_at)
+);
