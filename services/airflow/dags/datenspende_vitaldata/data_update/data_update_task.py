b484e8a4810a159033fb0af5950e43c7c0bc6820
David Hinrichs <david.hinrichs@pm.me>
2021-11-10 12:24:28 +0100

    fix up tests for datenspende.vitaldata and datenspende.users UPSERT

--- services/airflow/dags/datenspende_vitaldata/data_update/data_update_task.py
+++ services/airflow/dags/datenspende_vitaldata/data_update/data_update_task.py
@@ -6,10 +6,10 @@ from .upload import upload
 from dags.helpers.test_helpers import set_env_variable_from_dag_config_if_present
 
 
-DATA_UPDATE_ARGS = [os.environ["THRYVE_FTP_URL"] + "export.7z", "datenspende", "user_id, date, vital_id, device_id"]
+DATA_UPDATE_ARGS = [os.environ["THRYVE_FTP_URL"] + "export.7z", "datenspende"]
 
 
-def data_update_etl(url: str, schema: str, constraint: str, **kwargs):
+def data_update_etl(url: str, schema: str, **kwargs):
     R.pipe(
         set_env_variable_from_dag_config_if_present("TARGET_DB"),
         lambda *args: download(
@@ -20,6 +20,6 @@ def data_update_etl(url: str, schema: str, constraint: str, **kwargs):
             },
             url,
         ),
-        R.map(lambda item: (item[0], transform(item[1]))),
-        upload(schema, constraint),
+        R.map(lambda item: (item[0], item[1], transform(item[2]))),
+        upload(schema),
     )(kwargs)
--- services/airflow/dags/datenspende_vitaldata/data_update/upload.py
+++ services/airflow/dags/datenspende_vitaldata/data_update/upload.py
@@ -4,11 +4,11 @@ from dags.datenspende.data_update.download import DataList
 
 
 @R.curry
-def upload(schema: str, constraint: str, data: DataList):
-    R.map(upload_item(schema, constraint), data)
+def upload(schema: str, data: DataList):
+    R.map(upload_item(schema), data)
 
 
 @R.curry
-def upload_item(schema, constraint, item):
-    (table, df) = item
+def upload_item(schema, item):
+    (table, constraint, df) = item
     connect_to_db_and_upsert_polars_dataframe(schema, table, constraint, df)
