d59492a97ba039a3f720881e576b528c40b9a3a8
jakobkolb <jakob.j.kolb@gmail.com>
2021-12-16 09:30:32 +0100

    Change datenspende dag names.

--- services/airflow/dags/datenspende/case_detection_features/test_construct_features_task.py
+++ services/airflow/dags/datenspende/case_detection_features/test_construct_features_task.py
@@ -14,7 +14,7 @@ def test_feature_task_on_one_off_survey_results(db_context: DBContext):
 
     # fill database
     run_task_with_url(
-        "datenspende",
+        "datenspende_surveys_v2",
         "gather_data_from_thryve",
         "http://static-files/thryve/exportStudy.7z",
     )
@@ -64,7 +64,7 @@ def test_feature_task_on_weeekly_survey_results(db_context: DBContext):
 
     # fill database
     run_task_with_url(
-        "datenspende",
+        "datenspende_surveys_v2",
         "gather_data_from_thryve",
         "http://static-files/thryve/exportStudy.7z",
     )
