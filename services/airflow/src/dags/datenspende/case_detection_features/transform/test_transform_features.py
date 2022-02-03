f7a15575ee03b3dfbbf6eda23f0be433c2798596
jakobkolb <jakob.j.kolb@gmail.com>
2022-02-03 11:03:28 +0100

    Fix combine columns for missing columns to avoid failures resulting from missing id 137 in test data.

--- services/airflow/src/dags/datenspende/case_detection_features/transform/test_transform_features.py
+++ services/airflow/src/dags/datenspende/case_detection_features/transform/test_transform_features.py
@@ -17,6 +17,7 @@ from src.dags.datenspende.case_detection_features.transform.transform_features i
     get_weekly_dates,
     unstack_multiple_choice_question,
     select_first_answer_by_question_ids,
+    elements_of_l1_that_are_in_l2,
 )
 from src.dags.datenspende.case_detection_features.parameters import (
     ONE_OFF_QUESTIONS,
@@ -46,7 +47,8 @@ def test_restructure_one_off_features(prepared_db):
     reference = pd.DataFrame(
         columns=FEATURE_ORDER_ONE_OFF, data=[FIRST_FEATURE_VALUES_ONE_OFF]
     )
-
+    print(features.head(n=1))
+    print(reference.values)
     pd.testing.assert_frame_equal(features.head(n=1), reference, check_dtype=False)
 
 
@@ -215,6 +217,11 @@ def test_combine_columns():
 
 def test_get_symptom_ids_from_db(prepared_db):
     assert get_symptom_ids_from_weekly() == ONE_OFF_SYMPTOM_IDS
+
+
+def test_elements_of_l1_that_are_in_l2_returns_subset():
+    lst = elements_of_l1_that_are_in_l2([1, 2], [2, 3, 4])
+    assert lst == [2]
 
 
 @pytest.fixture
