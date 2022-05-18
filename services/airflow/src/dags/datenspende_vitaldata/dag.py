a75497c1ac26e61ed6dee3e9866fc2921bfd5166
David Hinrichs <dvd.hnrchs@gmail.com>
2022-05-18 22:22:04 +0200

    fix code quality

--- services/airflow/src/dags/datenspende_vitaldata/dag.py
+++ services/airflow/src/dags/datenspende_vitaldata/dag.py
@@ -128,7 +128,8 @@ t10 = BashOperator(
         "MODELS": "datenspende",  # this defines the models/sql files that are executed by dbt
         "TARGET_DB_SCHEMA": "datenspende_derivatives",  # this defines the schema in which dbt outputs arrive,
         "DBT_DIR": "/opt/airflow/src/dbt/",
-        **os.environ,  # TODO necessary to carry over existing env vars, fix this by updating Airflow to >2.3 and setting the flag append_env = True
+        **os.environ,  # TODO necessary to carry over existing env vars,
+                       # fix this by updating Airflow to >2.3 and setting the flag append_env = True
     },
 )
 
@@ -140,7 +141,8 @@ t11 = BashOperator(
         "MODELS": "datenspende",  # this defines the models/sql files that are executed by dbt
         "TARGET_DB_SCHEMA": "datenspende_derivatives",  # this defines the schema in which dbt outputs arrive,
         "DBT_DIR": "/opt/airflow/src/dbt/",
-        **os.environ,  # TODO necessary to carry over existing env vars, fix this by updating Airflow to >2.3 and setting the flag append_env = True
+        **os.environ,  # TODO necessary to carry over existing env vars,
+                       # fix this by updating Airflow to >2.3 and setting the flag append_env = True
     },
 )
 
