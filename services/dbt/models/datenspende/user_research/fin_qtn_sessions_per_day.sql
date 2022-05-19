33b026d30af239d70adb60112e5c2139c88c1382
David Hinrichs <dvd.hnrchs@gmail.com>
2022-05-19 12:59:56 +0200

    add documentation, clean up models

---
+++ services/dbt/models/datenspende/user_research/fin_qtn_sessions_per_day.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='view') }}
+
+with fin_qtn_sessions_per_day as (
+
+SELECT To_timestamp(completed_at / 1000) :: date AS cdate,
+       name                                      AS study_name,
+       Count(user_id)                            AS fin_qtn_sessions
+FROM   datenspende.questionnaire_session
+       inner join datenspende.study_overview
+               ON datenspende.questionnaire_session.study =
+                  datenspende.study_overview.id
+WHERE  completed_at IS NOT NULL
+GROUP  BY study_name, cdate
+ORDER  BY cdate 
+
+)
+
+select *
+from fin_qtn_sessions_per_day
