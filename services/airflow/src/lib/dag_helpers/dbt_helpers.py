9ae5e251d0e7ad2f5f4c4975475983d53d2e3b25
jakobkolb <jakob.j.kolb@gmail.com>
2022-06-14 23:24:03 +0200

    Improve logging from run_dbt_models.

--- services/airflow/src/lib/dag_helpers/dbt_helpers.py
+++ services/airflow/src/lib/dag_helpers/dbt_helpers.py
@@ -3,14 +3,19 @@ from src.lib.test_helpers import (
     set_env_variable_from_dag_config_if_present,
     set_env_variable,
 )
+import logging
+from airflow.exceptions import AirflowException
+
+log = logging.getLogger()
 
 
-def run_dbt_models(models: str, target_db_schema: str, **kwargs) -> None:
+def run_dbt_models(models: str, target_db_schema: str, dir: str, **kwargs) -> None:
     """Run the specified dbt models with output in target_db_schema"""
+
     set_env_variable_from_dag_config_if_present("TARGET_DB", kwargs)
     set_env_variable("DBT_LOGS", "/opt/airflow/logs/dbt/")
     set_env_variable("TARGET_DB_SCHEMA", target_db_schema)
-    result = subprocess.run(
+    sp = subprocess.Popen(
         [
             "dbt",
             "run",
@@ -21,8 +26,17 @@ def run_dbt_models(models: str, target_db_schema: str, **kwargs) -> None:
             "--profiles-dir",
             "/opt/airflow/dbt/",
         ],
-        capture_output=True,
-        text=True,
-        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        cwd=dir,
+        close_fds=True,
     )
-    print(result.stdout)
+    log.info("Output:")
+    for line in iter(sp.stdout.readline, b""):
+        line = line.decode("utf-8").rstrip()
+        log.info(line)
+    sp.wait()
+    log.info("Command exited with return code %s", sp.returncode)
+
+    if sp.returncode:
+        raise AirflowException("dbt command failed")
